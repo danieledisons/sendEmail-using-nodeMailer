73b7e532a054363956acebbc9703d00ac1e65118
danieledison <kanalt2700@gmail.com>
2023-11-04 12:34:22 +0100

    added CORS to backend

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 const { sendMail } = require("./app");
+
+app.use(cors());
+
+const data = {
+  status: 200,
+  message: "Email sent successfully",
+};
 
 app.get("/", (req, res) => {
   res.send("use /send/:email to send email...");
 });
 
 app.get("/send/:email", async (req, res) => {
-  const data = {
-    status: 200,
-    message: "Email sent successfully",
-  };
   try {
     if (req.params.email) {
       const info = await sendMail(req.params.email);
@@ -27,6 +32,6 @@ app.get("/send", (req, res) => {
   res.send("you are in the send route");
 });
 
-app.listen(3000, () => {
-  console.log("server listening on port 3000");
+app.listen(3001, () => {
+  console.log("server listening on port 3001");
 });
